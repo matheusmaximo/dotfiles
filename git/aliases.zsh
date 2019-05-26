1aa9c0d842874353c07f4811569e64d4f9f60d32
Kyle Ellman <kyle.ellman@gmail.com>
2019-05-25 20:22:47 -0700

    some git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,13 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gl='git log'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias g='git'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -22,3 +22,8 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias gh='github'
+alias gg='git browse'
+alias gpu='git push -u origin $(git symbolic-ref --short -q HEAD)'
+alias gbye-o="git-pull-master-and-delete-current-branch"
+alias gbye-u="git-pull-master-and-delete-current-branch upstream"
