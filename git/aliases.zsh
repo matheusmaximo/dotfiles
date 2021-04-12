996f18a203f01fd527407b168b1d8f8209987554
Kyle Ellman <kyle.ellman@gmail.com>
2021-04-11 19:35:51 -0700

    gcob and opctl aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ alias g='git'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcob='git checkout -b'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
--- opctl/aliases.zsh
+++ opctl/aliases.zsh
@@ -1,1 +1,2 @@
 alias or="opctl run"
+alias opctl-clear-cache="opctl node kill && sudo rm -rf ~/Library/Application\ Support/opctl/dcg"
