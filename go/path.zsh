802fb4094c9270f2c87be2b06ec5a84afcb33205
Kyle Ellman <kyle.ellman@gmail.com>
2021-08-16 15:20:27 -0700

    don't forget to xargs

--- go/path.zsh
+++ go/path.zsh
@@ -1,7 +1,7 @@
 if [ -x "$(which go)" ]; then
-	export $(go env | grep GOCACHE)
-	export $(go env | grep GOMODCACHE)
+	export $(go env | grep GOCACHE | xargs)
+	export $(go env | grep GOMODCACHE | xargs)
 
-	export $(go env | grep GOPATH)
+	export $(go env | grep GOPATH | xargs)
 	export PATH="$GOPATH/bin:$PATH"
 fi
