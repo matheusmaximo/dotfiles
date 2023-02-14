0eaf27028ecd920cb11609c57363273fab248eb9
kyleellman <kyle.ellman@gmail.com>
2023-02-14 10:17:23 -0800

    Updating after moving to m1, dropping some unecessary things

--- opctl/install.sh
+++ opctl/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ### TODO don't do the curl thing if it's already installed
-test -e /usr/local/bin/opctl || curl -L https://github.com/opctl/opctl/releases/latest/download/opctl-darwin-amd64.tgz | tar -xzv -C /usr/local/bin
+test -e /usr/local/bin/opctl || curl -L https://github.com/opctl/opctl/releases/latest/download/opctl-darwin-arm64.tgz | tar -xzv -C /usr/local/bin
 opctl self-update
