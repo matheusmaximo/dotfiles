53a16d45ad93c2b6dba3617f4b1721fd771adfe3
kyleellman <kyle.ellman@gmail.com>
2023-09-13 14:47:25 -0700

    opctl self-update wants sudo

--- opctl/install.sh
+++ opctl/install.sh
@@ -2,4 +2,4 @@
 
 ### TODO don't do the curl thing if it's already installed
 test -e /usr/local/bin/opctl || curl -L https://github.com/opctl/opctl/releases/latest/download/opctl-darwin-arm64.tgz | tar -xzv -C /usr/local/bin
-opctl self-update
+sudo opctl self-update
