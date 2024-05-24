a926070c4538b1f066e573a19685b85cc75dcb9a
Matheus Araujo <12360888+matheusmaximo@users.noreply.github.com>
2024-05-24 14:31:28 +0100

    Add alias to auth aws

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias authaws='forge auth aws --role DeveloperAccess --role cstx-dev-role --account "Remitly Product"'
